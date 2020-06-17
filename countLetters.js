8a55c417cf40f5981ed57b1d170bad64c5946807
MargaritaSt <margarita.struts@me.com>
2020-06-17 21:55:37 +0000

    Add countLetters and countOnly

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+/// assertEqual 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed:  ${actual}  !==  ${expected}`);
+  }
+};
+/// countOnly
+const countLetters = function(allItems) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (item !== ' ') { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("lighthouse in the house");
+assertEqual(result1["l"], 2);
+assertEqual(result1["z"], undefined);
+assertEqual(result1["i"], 2);
