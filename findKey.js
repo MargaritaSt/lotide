37f81bef6b78dc1199f8cafdb409dbce201a1aed
MargaritaSt <margarita.struts@me.com>
2020-06-19 04:34:24 +0000

    add findekey

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed:  ${actual}  !==  ${expected}`);
+  }
+};
+
+const findKey = function(array,callback) {
+  let keys = Object.keys(array);
+  for (let key of keys) {
+    if (callback(array[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),'nom');
