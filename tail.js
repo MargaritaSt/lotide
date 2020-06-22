10b1773bcfd4f13e6f5c2c757d8543f99924911a
MargaritaSt <margarita.struts@me.com>
2020-06-22 21:07:24 +0000

    tail

--- tail.js
+++ tail.js
@@ -11,26 +11,15 @@ const assertEqual = function(actual, expected) {
       console.log(`✅✅✅ Assertion Passed:  ${actual}   ===  ${expected}`);
     } else  {
       console.log(`❌❌❌ Assertion Failed:  ${actual}  !==  ${expected}`);
-    } 
+    }
   }
 };
 
 const tail = function(elements) {
   let newArray = [];
-  console.log(elements.length);
   for (let i = 1; i < elements.length; i++) {
     newArray.push(elements[i]);
   }
   return newArray;
 };
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-
-
-
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,4 +2,3 @@ const assertEqual = require('../assertEqual');
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
