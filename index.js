34512461c45f0280dab38dd14f60acc0f1f04025
MargaritaSt <margarita.struts@me.com>
2020-06-23 02:41:40 +0000

    add index

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const map = require('./map');
+const letterPosition = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  map: map,
+  letterPosition: letterPosition,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  without: without,
+  takeUntil: takeUntil
+};
